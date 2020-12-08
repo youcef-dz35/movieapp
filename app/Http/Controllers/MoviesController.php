fe438b492fa26aeccc911146b07d7667615f7f78
KEBIR Youcef <kebiryoucef07@gmail.com>
2020-12-08 16:54:37 +0100

    api auth 3

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -20,14 +20,14 @@ class MoviesController extends Controller
                                ->get('https://api.themoviedb.org/3/movie/popular')
                                ->json()['results'];
         //dd($popularMovies);
-        $genres =  Http::withToken(config('services.tmdb.token'))
+        $genres =  Http::withToken(config('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2Q4ZmFkZGM4MzYwMGYyOWNjZWM1NTExMmM3YWZiOCIsInN1YiI6IjVmYzIzZjcwMmJjZjY3MDAzZmJmNzQwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SqGRXpQo_THmUtoLRYYOO-euu6TCtM2XqJ6JVYY2CAI'))
                         ->get('https://api.themoviedb.org/3/genre/movie/list')
                         ->json()['genres'];
         //
         // $genres = \collect($genresArray)->mapWithKeys( function ($genres){
         //     return [$genres['id'] => $genres['name']];
         // });
-        $nowPlayingMovies =  Http::withToken(config('services.tmdb.token'))
+        $nowPlayingMovies =  Http::withToken(config('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2Q4ZmFkZGM4MzYwMGYyOWNjZWM1NTExMmM3YWZiOCIsInN1YiI6IjVmYzIzZjcwMmJjZjY3MDAzZmJmNzQwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SqGRXpQo_THmUtoLRYYOO-euu6TCtM2XqJ6JVYY2CAI'))
                         ->get('https://api.themoviedb.org/3/movie/now_playing')
                         ->json()['results'];
         //\dd($genres);
@@ -80,7 +80,7 @@ class MoviesController extends Controller
     {
 
 
-        $movie = Http::withToken(config('services.tmdb.token'))
+        $movie = Http::withToken(config('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2Q4ZmFkZGM4MzYwMGYyOWNjZWM1NTExMmM3YWZiOCIsInN1YiI6IjVmYzIzZjcwMmJjZjY3MDAzZmJmNzQwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SqGRXpQo_THmUtoLRYYOO-euu6TCtM2XqJ6JVYY2CAI'))
                 ->get('https://api.themoviedb.org/3/movie/'.$id.'?append_to_response=credits,videos,images')
                 ->json();
         $viewModel = new MovieViewModel(
